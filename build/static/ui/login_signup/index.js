a8a3f9ce2cb2430a9e59d0149fda73e0d200fcd4
Dheeraj Raina <dheerajraina55@gmail.com>
2024-12-04 12:08:57 +0530

    basic handeling of createAccount event

--- build/static/ui/login_signup/index.js
+++ build/static/ui/login_signup/index.js
@@ -1,6 +1,7 @@
 const signUpBUtton = document.getElementById("signUp");
 const signInBUtton = document.getElementById("signIn");
 const container = document.getElementById("container");
+const createAccountButton = document.getElementById("createAccount");
 
 // switch between login and signup
 
@@ -11,3 +12,34 @@ signUpBUtton.addEventListener("click", () => {
 signInBUtton.addEventListener("click", () => [
         container.classList.remove("right-panel-active")
 ]);
+
+createAccountButton.addEventListener("click", async () => {
+
+        console.log("create Account");
+        const username = document.getElementById("createAccount_username").value;
+        const password = document.getElementById("createAccount_password").value;
+        const inputData = { "username": username, "password": password };
+        console.log(inputData);
+        try {
+
+                const response = await fetch("http://localhost:8080/register", {
+                        method: "POST",
+                        headers: {
+                                'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(inputData)
+                });
+                if (!response.ok) {
+                        const errorText = await response.text();
+                        throw new Error("Server Error:" + errorText);
+                }
+                const data = await response.text();
+                console.log(data);
+        } catch (error) {
+                console.log(error.message);
+
+        }
+
+
+
+});
