5c6ca9141fd03f3fcf11ce4e4576cfccc782787c
Dheeraj Raina <dheerajraina55@gmail.com>
2024-12-05 14:07:27 +0530

    basic handeling of login event in the ui

--- build/static/ui/login_signup/index.js
+++ build/static/ui/login_signup/index.js
@@ -1,8 +1,8 @@
 const signUpBUtton = document.getElementById("signUp");
 const signInBUtton = document.getElementById("signIn");
 const container = document.getElementById("container");
-const createAccountButton = document.getElementById("createAccount");
-
+const createAccountButton = document.getElementById("createAccountButton");
+const loginButton = document.getElementById("loginButton");
 // switch between login and signup
 
 signUpBUtton.addEventListener("click", () => {
@@ -35,6 +35,43 @@ createAccountButton.addEventListener("click", async () => {
                 }
                 const data = await response.text();
                 console.log(data);
+        } catch (error) {
+                console.log(error.message);
+
+        }
+
+
+
+});
+
+
+loginButton.addEventListener("click", async () => {
+
+        console.log("login into account");
+        const username = document.getElementById("login_username").value;
+        const password = document.getElementById("login_password").value;
+        const inputData = { "username": username, "password": password };
+        console.log(inputData);
+        try {
+
+                const response = await fetch("http://localhost:8080/login", {
+                        method: "POST",
+                        headers: {
+                                'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(inputData)
+                });
+                if (!response.ok) {
+                        const errorText = await response.text();
+                        throw new Error("Server Error:" + errorText);
+                }
+                const data = await response.text();
+                console.log(data);
+
+                // JWT token storage either in  localstorage or session storage
+
+
+                location.replace("/web/chat");
         } catch (error) {
                 console.log(error.message);
 
