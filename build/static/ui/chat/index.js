d457cc7e0b320ae63bf08a5bae85147ccad202f6
Dheeraj Raina <dheerajraina55@gmail.com>
2025-04-24 12:59:29 +0530

    UI update

--- build/static/ui/chat/index.js
+++ build/static/ui/chat/index.js
@@ -1,14 +1,14 @@
-let ws;
+// let ws;
 
-var element = document.querySelector(".message-container");
-element.scrollTop = element.scrollHeight;
+// var element = document.querySelector(".message-container");
+// element.scrollTop = element.scrollHeight;
 
-let open = document.querySelector("#open");
-let close = document.querySelector(".close");
-let panel = document.querySelector(".contact-detail");
-let msgSendButton = document.getElementById("send");
-let msgText = document.getElementById("message-input");
-let msgContainer = document.getElementById("message-container");
+// let open = document.querySelector("#open");
+// let close = document.querySelector(".close");
+// let panel = document.querySelector(".contact-detail");
+// let msgSendButton = document.getElementById("send");
+let msgText = document.getElementById("input-message");
+let msgContainer = document.getElementById("messages");
 
 
 window.onload = () => {
@@ -18,9 +18,9 @@ window.onload = () => {
 
                 ws.onmessage = (event) => {
                         const messageDiv = document.createElement('div');
-                        messageDiv.className = 'comment other';
-                        const textMsgBubble = document.createElement('div');
-                        textMsgBubble.className = 'bubble';
+                        messageDiv.className = 'msg-text';
+                        const textMsgBubble = document.createElement('span');
+                        textMsgBubble.className = 'text';
                         textMsgBubble.textContent = JSON.parse(event.data).content;
 
                         messageDiv.appendChild(textMsgBubble);
@@ -39,56 +39,151 @@ window.onload = () => {
 
 
 
-open.onclick = function () {
-        gsap.set(panel, {
-                display: "block",
-                x: 300
-        });
-        gsap.to(panel, 0.5, { x: 0 });
-};
+// open.onclick = function () {
+//         gsap.set(panel, {
+//                 display: "block",
+//                 x: 300
+//         });
+//         gsap.to(panel, 0.5, { x: 0 });
+// };
 
-close.onclick = function () {
-        gsap.to(panel, 0.5, { x: 320 });
-};
+// close.onclick = function () {
+//         gsap.to(panel, 0.5, { x: 320 });
+// };
 
-let openList = document.querySelector(".open-message");
-let closeList = document.querySelector(".close-list");
-let list = document.querySelector(".message-list");
+// let openList = document.querySelector(".open-message");
+// let closeList = document.querySelector(".close-list");
+// let list = document.querySelector(".message-list");
 
-openList.onclick = function () {
-        gsap.set(list, { display: "block", x: -300 });
-        gsap.to(list, 0.5, { x: 0 });
-};
+// openList.onclick = function () {
+//         gsap.set(list, { display: "block", x: -300 });
+//         gsap.to(list, 0.5, { x: 0 });
+// };
 
-closeList.onclick = function () {
-        gsap.to(list, 0.5, { x: -400 });
-        gsap.to(list, 0, { display: "none", delay: 1 });
-};
+// closeList.onclick = function () {
+//         gsap.to(list, 0.5, { x: -400 });
+//         gsap.to(list, 0, { display: "none", delay: 1 });
+// };
 
-msgSendButton.addEventListener("click", () => {
-        if ((msgText.value).length > 0) {
+// msgSendButton.addEventListener("click", () => {
+//         if ((msgText.value).length > 0) {
 
-                try {
-                        ws.send(JSON.stringify({ from: "user1", to: "user2", content: msgText.value }));
+//                 try {
+//                         ws.send(JSON.stringify({ from: "user1", to: "user2", content: msgText.value }));
 
-                        const messageDiv = document.createElement('div');
-                        messageDiv.className = 'comment me';
-                        const textMsgBubble = document.createElement('div');
-                        textMsgBubble.className = 'bubble';
-                        textMsgBubble.textContent = msgText.value;
+//                         const messageDiv = document.createElement('div');
+//                         messageDiv.className = 'comment me';
+//                         const textMsgBubble = document.createElement('div');
+//                         textMsgBubble.className = 'bubble';
+//                         textMsgBubble.textContent = msgText.value;
 
-                        messageDiv.appendChild(textMsgBubble);
-                        msgContainer.appendChild(messageDiv);
-                        msgText.value = '';
-                        msgContainer.scrollTop = msgContainer.scrollHeight;
+//                         messageDiv.appendChild(textMsgBubble);
+//                         msgContainer.appendChild(messageDiv);
+//                         msgText.value = '';
+//                         msgContainer.scrollTop = msgContainer.scrollHeight;
 
-                } catch (error) {
+//                 } catch (error) {
 
-                        console.log(error.message);
-                }
+//                         console.log(error.message);
+//                 }
 
 
+//         }
+
+
+// });
+
+
+
+//Selector
+const $ = (selector) => document.querySelector(selector);
+
+//Chat
+const chat = $('#chat');
+
+//Options
+const options = $('#options'),
+        button_options = $('#button-options'),
+        close_options = $('#close-options');
+
+button_options.addEventListener('click', () => {
+        options.classList.toggle('active');
+});
+
+close_options.addEventListener('click', () => {
+        options.classList.remove('active');
+});
+
+//Dark Mode
+const toggle = $('#input-dark');
+
+toggle.addEventListener('change', function () {
+        this.checked ? chat.classList.add('dark') : chat.classList.remove('dark');
+});
+
+//Colors
+const blue = $('#color-blue'),
+        red = $('#color-red'),
+        green = $('#color-green'),
+        purple = $('#color-purple');
+
+blue.addEventListener('click', () => {
+        delete chat.dataset.color;
+});
+red.addEventListener('click', () => {
+        chat.dataset.color = 'red';
+});
+green.addEventListener('click', () => {
+        chat.dataset.color = 'green';
+});
+purple.addEventListener('click', () => {
+        chat.dataset.color = 'purple';
+});
+
+//Messages
+const messages = $('#messages'),
+        input = $('#input-message'),
+        send = $('#send-text');
+
+//Send messages
+input.addEventListener('keypress', function (ev) {
+        if (ev.key === 'Enter' && input.value) {
+                ev.preventDefault();
+                const msg = input.value;
+                sendMessage(msg);
+        };
+});
+
+send.addEventListener('click', () => {
+        const msg = input.value;
+        if (msg) sendMessage(msg);
+});
+
+const sendMessage = (msg) => {
+        //Create message
+        const block = document.createElement('div');
+        block.setAttribute('class', 'msg-text owner');
+        const message = document.createElement('span');
+        message.setAttribute('class', 'text');
+        message.innerText = String(msg);
+
+        //Append message
+        block.appendChild(message);
+        messages.appendChild(block);
+        messages.scrollTo(0, 99999);
+
+        try {
+                ws.send(JSON.stringify({ from: "user1", to: "user2", content: msgText.value }));
+
+
+        } catch (error) {
+
+                console.log(error.message);
         }
 
 
-});
+        //Clear input
+        input.value = '';
+        input.focus();
+
+};
